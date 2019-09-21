18a8400760809a2691dafe655ab6416b412405bc
Oscar Rodriguez <geekscar@users.noreply.github.com>
2019-09-21 12:07:04 -0500

    Usando Datos Dinamicos de un dict en el template

--- main_app/views.py
+++ main_app/views.py
@@ -4,5 +4,14 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
+    #return render(request, 'index.html')
     #return HttpResponse("<h1>Bienvenido a Automart</h1>")
+
+    nombre = "VW Jetta"
+    modelo = 2018
+    precio = 145000
+    color = "Rojo"
+
+    context = {'auto_nombre' : nombre, 'auto_modelo' : modelo, 'auto_precio' : precio, 'auto_color' : color}
+
+    return render(request, 'index.html', context)
